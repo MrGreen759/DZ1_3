15be24c7bfc43ee779930a068e0167a90dba181c
Alexey Zelenkov <alan.lapsky@gmail.com>
2022-09-08 11:23:01 +0300

    Add solution of Task2 and README

--- Comission2/src/main/kotlin/Main.kt
+++ Comission2/src/main/kotlin/Main.kt
@@ -4,38 +4,44 @@ fun main() {
     var toCard: String              // тип карты, на которую переводят
     var sentInMonth: Int = 0        // переведено за месяц
 
-    //println("Комиссия за перевод составит ${comission(value, fromCard, toCard)} копеек.")
+    value = 60000
+    fromCard = "Visa"
+    toCard = "VK Pay"
+    println (comission(value, fromCard, toCard, sentInMonth))
 
+    value = 60000
+    fromCard = "Mastercard"
+    toCard = "Visa"
+    sentInMonth = 21000
+    println (comission(value, fromCard, toCard, sentInMonth))
+
+    value = 60000
+    fromCard = "Мир"
+    toCard = "Visa"
+    println (comission(value, fromCard, toCard, sentInMonth))
 }
 
-fun comission (sum: Int, from: String, to: String, sent: Int): String {
+fun comission (sum: Int, from: String, to: String = "VK Pay", sent: Int = 0): String {
     var comissionValue: Int = 0
-    var result: String
+    var result: String = ""
 
     if ((sent+sum) > 600000) result = "Превышен месячный лимит."
     else {
         when {
             to == "VK Pay" -> result = "Комиссия не взимается."
             (from == "Visa") || (from == "Мир") -> {
-                comissionValue = (sum * 75) / 10000
+                comissionValue = (sum * 75) / 100
                 if (comissionValue < 3500 ) comissionValue = 3500
+                result = "Комиссия составит " + comissionValue.toString() + " копеек."
             }
             (from == "Mastercard") || (from == "Maestro") -> {
-
+                if ((sent + sum) <= 75000) result = "Комиссия не взимается"
+                else {
+                    comissionValue = (sum * 6) / 10 + 2000
+                    result = "Комиссия составит " + comissionValue.toString() + " копеек."
+                }
             }
         }
     }
-
-
-}
-
-fun checkLimit (sum: Int, from: String = "VK Pay", sentInMonth: Int, sentInDay: Int): Boolean {
-    if (from == "VK Pay") {
-        if (sum > 15000) return false
-        if ((sentInDay+sum) > 40000) return false
-        return true
-    }
-    if ((sentInDay + sum) > 150000) return false
-    if ((sentInMonth + sum) > 600000) return false
-    return true
+    return result
 }
